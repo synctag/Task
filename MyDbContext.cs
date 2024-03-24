2c1f1eb130abac276a96126fa65397a2b77feb63
synctag <164305309+synctag@users.noreply.github.com>
2024-03-25 04:15:50 +0530

    Add files via upload

---
+++ MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApplication.Model;
+
+namespace TestApplication.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<CarDetails> CarDetails { get; set; }   
+         public MyDbContext(DbContextOptions dbContextOptions): base(dbContextOptions) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CarDetails>();
+        }
+    }
+}
+
