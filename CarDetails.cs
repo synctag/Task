1d3e01264062814f0484541ee015cb289c7af1bd
synctag <164305309+synctag@users.noreply.github.com>
2024-03-25 04:16:16 +0530

    Add files via upload

---
+++ CarDetails.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+//using System.ComponentModel.DataAnnotations;
+//using System.Runtime.InteropServices;
+
+namespace TestApplication.Model
+{
+    public class CarDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Make { get; set; }        
+        public string? Model { get; set; } 
+        public int Year { get; set; }     
+        public string? Trim { get; set; }
+        public string? Engine { get; set; }
+
+        public bool? Status { get; set; }   
+
+
+
+    }
+    public class CarDetailReq
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int Year { get; set; }
+        public string? Trim { get; set; }
+        public string? Engine { get; set; }
+
+        public bool? Status { get; set; }
+
+
+
+    }
+}
